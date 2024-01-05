77ff78c79ad00d799ab8c7f24b781956804b2f47
Alishakathania <155672311+Alishakathania@users.noreply.github.com>
2024-01-05 19:13:40 +0000

    variables

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,24 @@
+const accountId = 43243
+let accountEmail = "alisha@google.com"
+var accountAddress = "mandi"
+accountPassword = 1234
+
+// accountId =  2  //not allowed
+
+console.log(accountId);
+
+accountEmail = "alisha@star.com"
+accountAddress = "manali"
+accountPassword = 312234
+let accountstate;      // here i am not defining its value..
+
+console.table([accountId,accountAddress,accountEmail,accountPassword,accountstate]);
+ // this can represent your output in the form of table..
+
+
+ /*
+    prefer not to use var
+    because of issue in block scope and functional scope
+ */
+
+
