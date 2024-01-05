e696564c4e35ea36a1aaedd08fc247cf63167b1f
Alishakathania <155672311+Alishakathania@users.noreply.github.com>
2024-01-05 19:13:59 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,26 @@
+"use strict"; // treats all js code as newer verison
+
+// alert(3+3)  // we are using nodejs , not browser . it'll throw an error
+
+
+let name = "alisha" //string
+let age = 19 //number
+let isLoggedIn = false //boolean
+
+// premitive datatypes.....
+
+//number => 2 to the power 53
+//bigint : this is only used for large number like in tradings ,stock market etc.
+//srting => ""
+//boolean => true/false
+//null => standalone value
+//undefined => 
+//symbol => used in react when e talk about unique we use symbol.
+
+
+//objects =>
+
+console.log(typeof "alisha"); // used to kow the type of value or datatype
+console.log(typeof age); //number 
+console.log(typeof null); //object
+console.log(typeof undefined); //undefined
